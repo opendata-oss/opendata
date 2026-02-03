e56edcf016ad83d723d46ddd117ab8d5c6ff27be
Rohan Desai <desai.p.rohan@gmail.com>
2026-02-03 08:30:12 -0800

    checkpoint

--- vector/src/write_coordinator/coordinator.rs
+++ vector/src/write_coordinator/coordinator.rs
@@ -1,7 +1,7 @@
 use std::ops::RangeInclusive;
 use std::sync::Arc;
-use std::time::{Duration};
-use tokio::time::{Interval, interval};
+use std::time::Duration;
+use tokio::time::{Instant, Interval, interval_at};
 
 use crate::write_coordinator::{Delta, FlushWatchers, Flusher, WriteCoordinatorHandle};
 
@@ -92,11 +92,7 @@ impl<D: Delta> WriteCoordinator<D> {
         let flush_watchers = flush_watermarks.watchers.clone();
         let (flush_tx, flush_rx) = tokio::sync::watch::channel(FlushTaskMsg::None);
         let (flush_result_tx, flush_result_rx) = tokio::sync::mpsc::unbounded_channel();
-        let flush_task = FlushTask::new(
-            flusher,
-            flush_rx,
-            flush_result_tx,
-        );
+        let flush_task = FlushTask::new(flusher, flush_rx, flush_result_tx);
         let flush_task_jh = tokio::spawn(async move { flush_task.run().await });
         let write_task = WriteCoordinatorTask {
             current_delta: delta,
@@ -116,7 +112,7 @@ impl<D: Delta> WriteCoordinator<D> {
             ctl_tx,
             flush_watchers,
         };
-        Self { handle, task_jh, }
+        Self { handle, task_jh }
     }
 
     pub(crate) fn handle(&self) -> WriteCoordinatorHandle<D> {
@@ -139,10 +135,12 @@ struct WriteCoordinatorTask<D: Delta> {
     flush_task_jh: tokio::task::JoinHandle<()>,
 }
 
-impl <D: Delta> WriteCoordinatorTask<D> {
+impl<D: Delta> WriteCoordinatorTask<D> {
     async fn run(mut self) {
         // TODO: track a target next time to flush and set it when writes come in
-        let mut flush_timer: Interval = interval(self.flush_interval);
+        // Use interval_at to delay the first tick by flush_interval
+        let mut flush_timer: Interval =
+            interval_at(Instant::now() + self.flush_interval, self.flush_interval);
 
         loop {
             tokio::select! {
@@ -201,9 +199,13 @@ impl <D: Delta> WriteCoordinatorTask<D> {
     fn flush_pending(&self) -> bool {
         let durable_watermark = self.watermarks.durable();
         match &*self.flush_tx.borrow() {
-            FlushTaskMsg::Flush {delta, epoch_range} => {
-                durable_watermark >= *epoch_range.end()
-            },
+            FlushTaskMsg::Flush {
+                delta: _,
+                epoch_range,
+            } => {
+                // Return true if there's a flush in progress that hasn't completed
+                durable_watermark < *epoch_range.end()
+            }
             FlushTaskMsg::None => false,
         }
     }
@@ -228,12 +230,12 @@ impl <D: Delta> WriteCoordinatorTask<D> {
         self.current_delta = D::init(new_image);
         self.delta_start_epoch = epoch_range_end;
 
-        self.flush_tx.send(
-            FlushTaskMsg::Flush {
+        self.flush_tx
+            .send(FlushTaskMsg::Flush {
                 delta: Arc::new(delta),
                 epoch_range: epoch_range_start..=epoch_range_end,
-            }
-        ).unwrap();
+            })
+            .unwrap();
 
         // Update flushed watermark
         self.watermarks.update_flushed(epoch_range_end);
@@ -242,7 +244,7 @@ impl <D: Delta> WriteCoordinatorTask<D> {
 
 enum FlushTaskMsg<D: Delta> {
     None,
-    Flush{
+    Flush {
         delta: Arc<D::ImmutableDelta>,
         epoch_range: RangeInclusive<u64>,
     },
@@ -274,7 +276,7 @@ struct FlushTask<D: Delta> {
     tx: tokio::sync::mpsc::UnboundedSender<FlushResult<D>>,
 }
 
-impl <D: Delta> FlushTask<D> {
+impl<D: Delta> FlushTask<D> {
     fn new(
         flusher: Box<dyn Flusher<D>>,
         rx: tokio::sync::watch::Receiver<FlushTaskMsg<D>>,
@@ -284,27 +286,319 @@ impl <D: Delta> FlushTask<D> {
     }
 
     async fn run(mut self) {
-        let last_flushed = 0u64;
+        let mut last_flushed = 0u64;
         loop {
             let (delta, epoch_range) = {
-                let msg = self.rx.wait_for(|msg| {
-                    match msg {
+                let msg = self
+                    .rx
+                    .wait_for(|msg| match msg {
                         FlushTaskMsg::None => false,
-                        FlushTaskMsg::Flush { delta, epoch_range } => {
-                            return *epoch_range.end() > last_flushed;
-                        }
-                    }
-                }).await.expect("coordinator hung up");
+                        FlushTaskMsg::Flush {
+                            delta: _,
+                            epoch_range,
+                        } => *epoch_range.end() > last_flushed,
+                    })
+                    .await
+                    .expect("coordinator hung up");
                 let FlushTaskMsg::Flush { delta, epoch_range } = msg.clone() else {
                     panic!("unreachable");
                 };
                 (delta.clone(), epoch_range)
             };
-            self.flusher.flush(delta.as_ref()).await.expect("flush failed");
-            self.tx.send(FlushResult {
-                delta,
-                epoch_range,
-            }).expect("coordinator hung up");
+            self.flusher
+                .flush(delta.as_ref())
+                .await
+                .expect("flush failed");
+            last_flushed = *epoch_range.end();
+            self.tx
+                .send(FlushResult { delta, epoch_range })
+                .expect("coordinator hung up");
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Result;
+    use async_trait::async_trait;
+    use bytes::Bytes;
+    use common::{BytesRange, Record, StorageIterator, StorageRead, StorageResult};
+    use std::sync::atomic::{AtomicUsize, Ordering};
+
+    // Mock types for testing
+    #[derive(Clone)]
+    struct MockDelta {
+        writes: Vec<i32>,
+        image_value: i32,
+    }
+
+    struct MockImage {
+        value: i32,
+    }
+
+    struct MockImmutableDelta {
+        writes: Vec<i32>,
+    }
+
+    impl Delta for MockDelta {
+        type Image = MockImage;
+        type Write = i32;
+        type ImmutableDelta = MockImmutableDelta;
+
+        fn init(image: Self::Image) -> Self {
+            Self {
+                writes: Vec::new(),
+                image_value: image.value,
+            }
+        }
+
+        fn apply(&mut self, writes: Vec<Self::Write>) -> Result<()> {
+            self.writes.extend(writes);
+            Ok(())
+        }
+
+        fn estimate_size(&self) -> usize {
+            self.writes.len() * std::mem::size_of::<i32>()
+        }
+
+        fn freeze(&self) -> (Self::ImmutableDelta, Self::Image) {
+            let sum: i32 = self.writes.iter().sum();
+            (
+                MockImmutableDelta {
+                    writes: self.writes.clone(),
+                },
+                MockImage {
+                    value: self.image_value + sum,
+                },
+            )
+        }
+    }
+
+    struct MockFlusher {
+        flush_count: Arc<AtomicUsize>,
+        flushed_writes: Arc<tokio::sync::Mutex<Vec<Vec<i32>>>>,
+    }
+
+    impl MockFlusher {
+        fn new() -> Self {
+            Self {
+                flush_count: Arc::new(AtomicUsize::new(0)),
+                flushed_writes: Arc::new(tokio::sync::Mutex::new(Vec::new())),
+            }
+        }
+    }
+
+    #[async_trait]
+    impl Flusher<MockDelta> for MockFlusher {
+        async fn flush(&self, delta: &MockImmutableDelta) -> Result<Arc<dyn StorageRead>> {
+            self.flush_count.fetch_add(1, Ordering::SeqCst);
+            self.flushed_writes.lock().await.push(delta.writes.clone());
+            // Return a dummy StorageRead - we don't actually use it in tests
+            Ok(Arc::new(MockStorageRead))
+        }
+    }
+
+    struct MockStorageRead;
+
+    struct EmptyIterator;
+
+    #[async_trait]
+    impl StorageIterator for EmptyIterator {
+        async fn next(&mut self) -> StorageResult<Option<Record>> {
+            Ok(None)
+        }
+    }
+
+    #[async_trait]
+    impl StorageRead for MockStorageRead {
+        async fn get(&self, _key: Bytes) -> StorageResult<Option<Record>> {
+            Ok(None)
+        }
+
+        async fn scan_iter(
+            &self,
+            _range: BytesRange,
+        ) -> StorageResult<Box<dyn StorageIterator + Send + 'static>> {
+            Ok(Box::new(EmptyIterator))
+        }
+    }
+
+    #[tokio::test]
+    async fn should_apply_writes_and_increment_epoch() {
+        // given
+        let image = MockImage { value: 0 };
+        let flusher = MockFlusher::new();
+        let coordinator = WriteCoordinator::<MockDelta>::new(
+            image,
+            100,
+            Duration::from_secs(60), // Long interval to avoid automatic flushes
+            Box::new(flusher),
+        );
+        let handle = coordinator.handle();
+
+        // when
+        let write_handle1 = handle.write(42).await.unwrap();
+        let write_handle2 = handle.write(100).await.unwrap();
+
+        // then
+        let epoch1 = write_handle1.epoch().await.unwrap();
+        let epoch2 = write_handle2.epoch().await.unwrap();
+
+        assert_eq!(epoch1, 1);
+        assert_eq!(epoch2, 2);
+    }
+
+    #[tokio::test]
+    async fn should_flush_on_manual_request() {
+        // given
+        let image = MockImage { value: 0 };
+        let flusher = MockFlusher::new();
+        let flush_count = flusher.flush_count.clone();
+        let flushed_writes = flusher.flushed_writes.clone();
+        let coordinator = WriteCoordinator::<MockDelta>::new(
+            image,
+            100,
+            Duration::from_secs(60),
+            Box::new(flusher),
+        );
+        let handle = coordinator.handle();
+
+        // when
+        handle.write(10).await.unwrap().epoch().await.unwrap();
+        handle.write(20).await.unwrap().epoch().await.unwrap();
+        handle.flush().await.unwrap();
+
+        // Wait for flush task to process
+        while flush_count.load(Ordering::SeqCst) < 1 {
+            tokio::time::sleep(Duration::from_millis(10)).await;
+        }
+
+        // then
+        assert_eq!(flush_count.load(Ordering::SeqCst), 1);
+        let writes = flushed_writes.lock().await;
+        assert_eq!(writes.len(), 1);
+        assert_eq!(writes[0], vec![10, 20]);
+    }
+
+    #[tokio::test]
+    async fn should_flush_automatically_after_interval() {
+        // given
+        let image = MockImage { value: 0 };
+        let flusher = MockFlusher::new();
+        let flush_count = flusher.flush_count.clone();
+        let flushed_writes = flusher.flushed_writes.clone();
+        let coordinator = WriteCoordinator::<MockDelta>::new(
+            image,
+            100,
+            Duration::from_millis(50), // Short interval
+            Box::new(flusher),
+        );
+        let handle = coordinator.handle();
+
+        // when
+        handle.write(5).await.unwrap();
+        handle.write(15).await.unwrap();
+
+        // Wait for the automatic flush
+        tokio::time::sleep(Duration::from_millis(150)).await;
+
+        // then
+        assert!(flush_count.load(Ordering::SeqCst) >= 1);
+        let writes = flushed_writes.lock().await;
+        assert!(!writes.is_empty());
+        assert_eq!(writes[0], vec![5, 15]);
+    }
+
+    #[tokio::test]
+    async fn should_accumulate_writes_across_flushes() {
+        // given
+        let image = MockImage { value: 0 };
+        let flusher = MockFlusher::new();
+        let flush_count = flusher.flush_count.clone();
+        let flushed_writes = flusher.flushed_writes.clone();
+        let coordinator = WriteCoordinator::<MockDelta>::new(
+            image,
+            100,
+            Duration::from_secs(60),
+            Box::new(flusher),
+        );
+        let handle = coordinator.handle();
+
+        // when - first batch (await epoch to ensure write is processed)
+        handle.write(1).await.unwrap().epoch().await.unwrap();
+        handle.write(2).await.unwrap().epoch().await.unwrap();
+        handle.flush().await.unwrap();
+
+        // Wait for flush to actually complete by polling the flush_count
+        while flush_count.load(Ordering::SeqCst) < 1 {
+            tokio::time::sleep(Duration::from_millis(10)).await;
+        }
+
+        // when - second batch (await epoch to ensure write is processed)
+        handle.write(3).await.unwrap().epoch().await.unwrap();
+        handle.write(4).await.unwrap().epoch().await.unwrap();
+        handle.flush().await.unwrap();
+
+        // Wait for second flush to complete
+        while flush_count.load(Ordering::SeqCst) < 2 {
+            tokio::time::sleep(Duration::from_millis(10)).await;
+        }
+
+        // then
+        let writes = flushed_writes.lock().await;
+        assert_eq!(writes.len(), 2, "expected 2 flushes, got {}", writes.len());
+        assert_eq!(writes[0], vec![1, 2]);
+        assert_eq!(writes[1], vec![3, 4]);
+    }
+
+    #[tokio::test]
+    async fn should_not_flush_when_no_writes() {
+        // given
+        let image = MockImage { value: 0 };
+        let flusher = MockFlusher::new();
+        let flush_count = flusher.flush_count.clone();
+        let coordinator = WriteCoordinator::<MockDelta>::new(
+            image,
+            100,
+            Duration::from_secs(60),
+            Box::new(flusher),
+        );
+        let handle = coordinator.handle();
+
+        // when - request flush without any writes
+        handle.flush().await.unwrap();
+        tokio::time::sleep(Duration::from_millis(50)).await;
+
+        // then
+        assert_eq!(flush_count.load(Ordering::SeqCst), 0);
+    }
+
+    #[tokio::test]
+    async fn should_update_watermarks_correctly() {
+        // given
+        let image = MockImage { value: 0 };
+        let flusher = MockFlusher::new();
+        let coordinator = WriteCoordinator::<MockDelta>::new(
+            image,
+            100,
+            Duration::from_secs(60),
+            Box::new(flusher),
+        );
+        let handle = coordinator.handle();
+
+        // when
+        let write_handle = handle.write(42).await.unwrap();
+        let epoch = write_handle.epoch().await.unwrap();
+
+        // then - applied watermark should be updated
+        assert_eq!(epoch, 1);
+
+        // Flush and verify flushed watermark updates
+        handle.flush().await.unwrap();
+        tokio::time::sleep(Duration::from_millis(50)).await;
+
+        // The watchers should reflect the flushed epoch
+        // We verify this indirectly through successful completion
     }
 }
